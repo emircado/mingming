45d0e75afe644a4e74611f98cf474c7cb5ee6d0a
emircado <emir.mercado@upd.edu.ph>
2014-04-01 22:45:15 +0800

    Timer update

--- minggame.py
+++ minggame.py
@@ -1,3 +1,5 @@
+#BACKEND IMPORTS
+from __future__ import division
 import threading
 
 #FRONTEND IMPORTS
@@ -19,9 +21,8 @@ GRAY      = pygame.Color( 127, 127, 127)
 LIGHTGRAY = pygame.Color( 200, 191, 231)
 
 screen_width = 800
-screen_height = 600
-panel_width = screen_width/3
-panel_height = 389/2
+x_pos = screen_width/3
+y_pos = 389/2
 
 class minggame:
 
@@ -45,74 +46,68 @@ class minggame:
 
 	def __prepare_resources(self):
 		self.__levels = {
-			1: level(800, 0, -5, 10),
-			2: level(800, 0, -5, 10),
-			3: level(800, 0, -5, 10),
-			4: level(800, 0, -5, 10),
-			5: level(800, 0, -5, 10),
-			6: level(800, 0, -5, 10),
-			7: level(800, 0, -5, 10),
-			8: level(800, 0, -5, 10),
-			9: level(800, 0, -5, 10),
-			10: level(800, 0, -5, 10),
-			11: level(800, 0, -5, 10),
-			12: level(800, 0, -5, 10),
-			13: level(800, 0, -5, 10),
-			14: level(800, 0, -5, 10),
-			15: level(800, 0, -5, 10)
+			1: level(10, 0, -5, 10),
+			2: level(10, 0, -5, 10),
+			3: level(10, 0, -5, 10),
+			4: level(10, 0, -5, 10),
+			5: level(10, 0, -5, 10),
+			6: level(10, 0, -5, 10),
+			7: level(10, 0, -5, 10),
+			8: level(10, 0, -5, 10),
+			9: level(10, 0, -5, 10),
+			10: level(10, 0, -5, 10),
+			11: level(10, 0, -5, 10),
+			12: level(10, 0, -5, 10),
+			13: level(10, 0, -5, 10),
+			14: level(10, 0, -5, 10),
+			15: level(10, 0, -5, 10)
 		}
 
 	def __draw_things(self):
 		self.__screen.fill(BLACK)
 
-		pygame.draw.line(self.__screen, GREEN, [0, 171], [self.__level.timer, 171], 30)
+		pygame.draw.line(self.__screen, GREEN, [0, 171], [self.__timer_len, 171], 30)
 
-		pygame.draw.line(self.__screen, LIGHTGRAY, [0, 211], [screen_width, 211], 1)
-		pygame.draw.rect(self.__screen, GRAY, [0, 212, panel_width, panel_height])
-		pygame.draw.line(self.__screen, LIGHTGRAY, [panel_width, 212], [panel_width, 211+panel_height], 1)
-		pygame.draw.rect(self.__screen, GRAY, [panel_width+1, 212, panel_width, panel_height])
-		pygame.draw.line(self.__screen, LIGHTGRAY, [2*panel_width, 212], [2*panel_width, 211+panel_height], 1)
-		pygame.draw.rect(self.__screen, GRAY, [2*(panel_width)+1, 212, panel_width, panel_height])
-		pygame.draw.line(self.__screen, LIGHTGRAY, [0, 212+panel_height], [screen_width, 212+panel_height], 1)
-		pygame.draw.rect(self.__screen, GRAY, [0, 213+panel_height, panel_width, panel_height])
-		pygame.draw.line(self.__screen, LIGHTGRAY, [panel_width, 213+panel_height], [panel_width, 211+2*panel_height], 1)
-		pygame.draw.rect(self.__screen, GRAY, [panel_width+1, 213+panel_height, panel_width, panel_height])
-		pygame.draw.line(self.__screen, LIGHTGRAY, [2*panel_width, 213+panel_height], [2*panel_width, 211+2*panel_height], 1)
-		pygame.draw.rect(self.__screen, GRAY, [2*(panel_width)+1, 213+panel_height, panel_width, panel_height])
-		pygame.draw.line(self.__screen, LIGHTGRAY, [0, 212], [0, 212+2*panel_height], 1)
-		pygame.draw.line(self.__screen, LIGHTGRAY, [0, 210+2*panel_height], [screen_width, 210+2*panel_height], 1)
-		pygame.draw.line(self.__screen, LIGHTGRAY, [799, 212], [799, 212+2*panel_height], 1)
+		pygame.draw.rect(self.__screen, GRAY, [0, 212, x_pos, y_pos])
+		pygame.draw.line(self.__screen, LIGHTGRAY, [x_pos, 212], [x_pos, 211+y_pos], 1)
+		pygame.draw.rect(self.__screen, GRAY, [x_pos+1, 212, x_pos, y_pos])
+		pygame.draw.line(self.__screen, LIGHTGRAY, [2*x_pos, 212], [2*x_pos, 211+y_pos], 1)
+		pygame.draw.rect(self.__screen, GRAY, [2*(x_pos)+1, 212, x_pos, y_pos])
+		pygame.draw.line(self.__screen, LIGHTGRAY, [0, 212+y_pos], [screen_width, 212+y_pos], 1)
+		pygame.draw.rect(self.__screen, GRAY, [0, 213+y_pos, x_pos, y_pos])
+		pygame.draw.line(self.__screen, LIGHTGRAY, [x_pos, 213+y_pos], [x_pos, 211+2*y_pos], 1)
+		pygame.draw.rect(self.__screen, GRAY, [x_pos+1, 213+y_pos, x_pos, y_pos])
+		pygame.draw.line(self.__screen, LIGHTGRAY, [2*x_pos, 213+y_pos], [2*x_pos, 211+2*y_pos], 1)
+		pygame.draw.rect(self.__screen, GRAY, [2*(x_pos)+1, 213+y_pos, x_pos, y_pos])
+		pygame.draw.line(self.__screen, LIGHTGRAY, [0, 212], [0, 212+2*y_pos], 1)
+		pygame.draw.line(self.__screen, LIGHTGRAY, [0, 210+2*y_pos], [screen_width, 210+2*y_pos], 1)
+		pygame.draw.line(self.__screen, LIGHTGRAY, [799, 212], [799, 212+2*y_pos], 1)
 		pygame.draw.rect(self.__screen, RED, [0, 187, screen_width, 24])
 
 		self.__screen.blit(self.__font.render("STAGE "+str(self.__lvlnum), True, WHITE), (200,200))
 		self.__screen.blit(self.__font.render("CURRENT "+str(self.__current), True, WHITE), (250,250))
 		self.__screen.blit(self.__font.render("TIME "+str(self.start_time), True, WHITE), (300,300))
 
-	def __start_timer(self):
+	def __reset_timer(self):
 		#timer things
-		self.timer_width = 16
+		self.countdown = self.__level.timer 	#10, 9, 8, ..., 0
+		self.frame_rate = 60					#60 frames per second
+		self.__timer_len = screen_width			#800 is the length of the screen
 		self.frame_count = 0
-		self.frame_rate = 60
 		self.start_time = 10
 
+	def __start_timer(self):
+		self.__reset_timer()
+
 		while not self.__done:
+			self.frame_count = (self.frame_count+1)%self.frame_rate
+			self.__timer_len-=(screen_width/self.__level.timer)/self.frame_rate
 
-			# Set up for the timer
-			# timer_width = 16 #width of the timer GUI
-			# frame_count = 0
-			# frame_rate = 60
-			# start_time = 10
-			# For the Countdown Timer:
-			total_seconds = self.start_time - (self.frame_count // self.frame_rate) #seconds
-			# TIME'S UP!!!!
-			# if total_seconds < 0:
-			# 	total_seconds = 0
-			# 	print "BOOOOM!!!!"
+			#1 second has passed
+			if self.frame_count == 0:
+				self.countdown-=1
 
-			self.__level.timer = self.__level.timer-1.30
-			self.frame_count += 1
-
-			self.__clock.tick(self.frame_rate) 
+			self.__clock.tick(self.frame_rate)
 
 	def __send_command(self, msg):
 		self.__host.send_game_command(msg)
@@ -146,6 +141,8 @@ class minggame:
 						self.__game_over = True
 						self.__host.stop_game('OVER')
 					self.__done = True
+				else:
+					self.__reset_timer()
 
 			#simulate timeout
 			elif valid == False:
@@ -156,7 +153,9 @@ class minggame:
 					self.__game_over = True
 					self.__host.stop_game('OVER')
 					self.__done = True
-			
+				else:
+					self.__reset_timer()			
+
 			self.__host.cv_game_front.release()
 
 	#process updates received by client. CLIENT ONLY
@@ -169,7 +168,6 @@ class minggame:
 
 			#process update
 			update = self.__host.for_game_front.pop()
-			print update
 			valid = None
 			if update == 'CURRENT++':
 				valid = True
@@ -214,6 +212,7 @@ class minggame:
 					self.__send_command('PFFFT')
 				
 			pygame.display.update()
+			# self.__clock.tick(self.frame_rate) 
 			
 		return self.__game_over
 
@@ -250,4 +249,5 @@ class minggame:
 						self.__send_command('PFFFT')
 
 			pygame.display.update()
+			# self.__clock.tick(self.frame_rate) 
 		return self.__game_over
